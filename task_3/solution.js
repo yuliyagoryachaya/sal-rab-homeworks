ef20565305dc74ec21be0fe3f96f45bc5e847e1a
Yulia Goryachaya <yuliya.goryachaya@mail.ru>
2023-04-05 20:11:19 +0300

    task 3 done

--- task_3/solution.js
+++ task_3/solution.js
@@ -15,7 +15,7 @@ function renderCartItem(item) {
     // например,
     // 5 × 500.00 ₽ = 2500 ₽
 
-    let itemCountText = ;
+    let itemCountText = "";
     itemCountText = itemCountText + product.count;
     itemCountText =  itemCountText + " × ";
     itemCountText = itemCountText + product.price;
