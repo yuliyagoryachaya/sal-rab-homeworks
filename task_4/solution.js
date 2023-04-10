177c39bbad6bdad348c41e597b6b497232970a36
Yulia Goryachaya <yuliya.goryachaya@mail.ru>
2023-04-10 21:45:22 +0300

    task 4 done

--- task_4/solution.js
+++ task_4/solution.js
@@ -1,17 +1,17 @@
 // Задание 4.1. Вывод карточек товаров
 
 function parseProducts(json) {
-let data = JSON.parse(json);
-return data.products;
+	let data = JSON.parse(json);
+	return data.products;
 
 }
 
 function renderProductsCards(json) {
-clearProducts();
-let products = parseProducts(json);
-let length = products.length;
-for (let i = 0; i < (length-1); i +=1) {
-addProduct(products[i]);
-}
+	clearProducts();
+	let products = parseProducts(json);
+	let length = products.length;
+	for (let i = 0; i < length; i += 1) {
+		addProduct(products[i]);
+	}
 
 }
