8cf75a850e7e58800e35b648e086d04333bf02c4
Yulia Goryachaya <yuliya.goryachaya@mail.ru>
2023-04-10 22:48:22 +0300

    task final done

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,19 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+let data = {
+order: {address, sum},
+client: {name, phone},
+goods: []
+}
+let countOfGoods = goods.length;
+for (let i = 0; i < countOfGoods; i++) {
+data.goods.push({title:goods[i].title, count:goods[i].count});
+}
+data.order.address = "ул. " + address.street + ", дом " + address.house + ", " + address.entrance + " подъезд, " + address.floor + " этаж, кв. " + address.flat;
+data.order.sum = sum;
+data.client = name + " " + phone;
+let jsonData = JSON.stringify({data:data});
+return jsonData;
 
-    let countOfGoods = goods.length;
-
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
-
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
 }
